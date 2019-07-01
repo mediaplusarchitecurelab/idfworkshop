6287d708eed41b1056d2dbdd678192d33e59d4c7
chifuresearch <45413636+chifuresearch@users.noreply.github.com>
2019-07-01 17:31:21 +0800

    upload_0701=0530

--- inc/main.js
+++ inc/main.js
@@ -208,7 +208,7 @@ function loaddata(url,lang){
                                             '</div>'+
                                             '<div class="row">'+
                                                 '<div class ="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>'+
-                                                '<div class ="col-xs-8 col-sm-8 col-md-8 col-lg-8" style="text-align: center;"><img src="'+dataen.schedule[i].pic+'" class="img-responsive center"></img></div>'+
+                                                '<div class ="col-xs-8 col-sm-8 col-md-8 col-lg-8 text-center"><img src="'+dataen.schedule[i].pic+'" class="img-responsive center"></img></div>'+
                                                 '<div class ="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>'+
                                             '</div>'+
                                         '</div></td>'+
